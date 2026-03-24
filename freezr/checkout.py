68d398158a35aec18a8f717d16870c05b3aa5e2f
Jon Marshall <jon.marshall@cruk.cam.ac.uk>
2026-03-24 19:11:15 +0000

    delete and move logic working

--- freezr/checkout.py
+++ freezr/checkout.py
@@ -7,25 +7,24 @@ from freezr.auth import login_required
 
 bp = Blueprint('checkout', __name__, url_prefix='/checkout')
 
-@bp.route('/out', methods=('GET', 'POST'))
+@bp.route('/out', methods=['POST'])
 @login_required
 def checkout():
-    if request.method == 'POST':
-        entry_id = request.form['entry_id']
-        db = get_db()
-        error = None
+    entry_id = request.form.get('entry_id')
+    db = get_db()
+    error = None
+    
+    if not entry_id:
+        error = 'You must select an item to check out.'
         
-        if not entry_id:
-            error = 'You must select an item to check out.'
-            
-        if error is None:
-            # We use auth_id in the WHERE clause so users can only delete their own items
-            db.execute(
-                'DELETE FROM entries WHERE id = ? AND auth_id = ?',
-                (entry_id, g.user['id'])
-            )
-            db.commit()
-            return redirect(url_for('index.index'))
-            
-        flash(error)
-    return render_template('checkout/out.html')
+    if error is None:
+        # We use auth_id in the WHERE clause so users can only delete their own items
+        db.execute(
+            'DELETE FROM entries WHERE id = ? AND auth_id = ?',
+            (entry_id, g.user['id'])
+        )
+        db.commit()
+        return redirect(url_for('index.index'))
+        
+    flash(error)
+    return redirect(url_for('index.index'))
--- freezr/move.py
+++ freezr/move.py
@@ -1,37 +1,33 @@
 import functools
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, request, url_for
 )
 from freezr.db import get_db
 from freezr.auth import login_required
 
 bp = Blueprint('move', __name__, url_prefix='/move')
 
-@bp.route('/about', methods=('GET', 'POST'))
+@bp.route('/do', methods=['POST'])
 @login_required
-def about():
-    if request.method == 'POST':
-        entry_id = request.form['entry_id']
-        new_freezer = request.form['freezer_id']
-        new_drawer = request.form['drawer']
+def move():
+    entry_id = request.form.get('entry_id')
+    freezer_id = request.form.get('freezer')
+    drawer = request.form.get('drawer')
+    
+    db = get_db()
+    error = None
+    
+    if not entry_id or not freezer_id or not drawer:
+        error = 'You must select an item, a freezer, and a drawer.'
         
-        db = get_db()
-        error = None
+    if error is None:
+        # Update the specific item making sure it belongs to the logged in user
+        db.execute(
+            'UPDATE entries SET freezer_id = ?, drawer = ? WHERE id = ? AND auth_id = ?',
+            (freezer_id, drawer, entry_id, g.user['id'])
+        )
+        db.commit()
+        return redirect(url_for('index.index'))
         
-        if not entry_id:
-            error = 'You must select an item to move.'
-        elif not new_freezer:
-            error = 'New freezer location is required.'
-        elif not new_drawer:
-            error = 'New drawer location is required.'
-            
-        if error is None:
-            db.execute(
-                'UPDATE entries SET freezer_id = ?, drawer = ? WHERE id = ? AND auth_id = ?',
-                (new_freezer, new_drawer, entry_id, g.user['id'])
-            )
-            db.commit()
-            return redirect(url_for('index.index'))
-            
-        flash(error)
-    return render_template('move/about.html')
+    flash(error)
+    return redirect(url_for('index.index'))
